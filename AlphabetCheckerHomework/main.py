87664d6719e1b211c21300b14c091704a57aeb68
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-13 21:35:38 +0530

    Add files via upload

---
+++ AlphabetCheckerHomework/main.py
@@ -0,0 +1,6 @@
+character = input("Enter the character")
+
+if character == 'a' or character == 'b' or character == 'c' or character == 'd' or character == 'e' or character == 'f' or character == 'g' or character == 'h' or character == 'i' or character == 'j' or character == 'k' or character == 'l' or character == 'm' or character == 'n' or character == 'o' or character == 'p' or character == 'q' or character == 'r' or character == 's' or character == 't' or character == 'u' or character == 'v' or character == 'w' or character == 'x' or character == 'y' or character == 'z' or character == 'A' or character == 'B' or character == 'C' or character == 'D' or character == 'E' or character == 'F' or character == 'G' or character == 'H' or character == 'I' or character == 'J' or character == 'K' or character == 'L' or character == 'M' or character == 'N' or character == 'O' or character == 'P' or character == 'Q' or character == 'R' or character == 'S' or character == 'T' or character == 'U' or character == 'V' or character == 'W' or character == 'X' or character == 'Y' or character == 'Z':
+    print("The character is an alphabet")
+else:
+    print("The character is not an alphabet")
